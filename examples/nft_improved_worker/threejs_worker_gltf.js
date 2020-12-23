718b69e86e97801c9f70a1bae44f1232bf341af6
CapSparkle <Feskovich.A@yandex.com>
2020-12-23 21:22:56 +0700

    testSiteSetup

--- examples/nft_improved_worker/threejs_worker_gltf.js
+++ examples/nft_improved_worker/threejs_worker_gltf.js
@@ -105,7 +105,7 @@ function start(container, marker, video, input_width, input_height, canvas_draw,
         model = gltf.scene.children[0];
 
         model.position.z = 0;
-        model.position.x = 50;
+        model.position.x = -100;
         model.position.y = -3000;
 
         model.scale.z = 200;
