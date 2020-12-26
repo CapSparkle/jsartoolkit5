ace96df75916714ecd356ec5e16060e87297028f
CapSparkle <Feskovich.A@yandex.com>
2020-12-26 18:43:13 +0700

    Update threejs_worker_gltf.js

--- examples/nft_improved_worker/threejs_worker_gltf.js
+++ examples/nft_improved_worker/threejs_worker_gltf.js
@@ -102,6 +102,10 @@ function start(container, marker, video, input_width, input_height, canvas_draw,
     );
 
     var root = new THREE.Object3D();
+    root.add(sphere);
+    sphere.position.set(0, 0, 0);
+
+
     scene.add(root);
 
     /* Load Model */
@@ -125,7 +129,7 @@ function start(container, marker, video, input_width, input_height, canvas_draw,
         action.play();
 
         root.matrixAutoUpdate = false;
-        //root.add(model);
+        root.add(model);
     });
 
     const listener = new THREE.AudioListener();
