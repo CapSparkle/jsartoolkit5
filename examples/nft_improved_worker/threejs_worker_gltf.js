5d0cefc8d7e2ebc9af864d58e8291a59be70372e
CapSparkle <Feskovich.A@yandex.com>
2020-12-28 16:58:10 +0700

    Update threejs_worker_gltf.js

--- examples/nft_improved_worker/threejs_worker_gltf.js
+++ examples/nft_improved_worker/threejs_worker_gltf.js
@@ -89,7 +89,7 @@ function start(container, marker, video, input_width, input_height, canvas_draw,
 
     //var camera = new THREE.Camera();
     //camera.matrixAutoUpdate = false;
-    var camera = new THREE.PerspectiveCamera(46, window.innerWidth / window.innerHeight, 1, 10000);
+    var camera = new THREE.PerspectiveCamera(41, window.innerWidth / window.innerHeight, 1, 10000);
     camera.position.z = 400;
 
     scene.add(camera);
